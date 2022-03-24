3140f5ae8eb5a3e8f2ce492ad28543410bb9abf2
pwhb <pwhbdev@gmail.com>
2022-03-24 23:23:21 +0630

    switched to webhook

--- index.js
+++ index.js
@@ -14,54 +14,60 @@ import mongoose from "mongoose";
 import express from "express";
 
 const app = express();
-const port = process.env.PORT || 4000;
+const { PORT, WEBHOOK_URL } = process.env
 
 app.get("/", (req, res) => res.send("runfree bot"));
-app.listen(port, () => {
-  console.log("server started");
-});
 
-async function main() {
+
+const bot = new Telegraf(process.env.BOT_TOKEN);
+// const bot = new Telegraf(process.env.TEST_TOKEN);
+
+bot.start((ctx) => {
+  ctx.reply(`hello ${ctx.chat.first_name}`);
+});
+bot.command("test", (ctx) => {
+  ctx.reply(ctx.chat.id);
+});
+// register
+bot.command("register", register);
+
+bot.command("why_join", why_join);
+
+// check id
+bot.command("my_id", my_info);
+
+bot.command("when", when);
+
+bot.command("help", help);
+
+bot.command("present", present);
+
+bot.command("class", class_info);
+
+bot.command("tell_a_joke", jokeHandler);
+
+bot.on("text", textHandler);
+
+const secretPath = `/telegraf/${bot.secretPathComponent()}`
+
+bot.telegram.setWebhook(`${WEBHOOK_URL}${secretPath}`)
+
+// Set the bot API endpoint
+app.use(bot.webhookCallback(secretPath))
+
+app.listen(PORT, () => {
   await mongoose
     .connect(process.env.MONGODB_URI)
     .then(() => {
       console.log("database connected!");
     })
     .catch((e) => console.log(e));
-  const bot = new Telegraf(process.env.BOT_TOKEN);
-  // const bot = new Telegraf(process.env.TEST_TOKEN);
+  console.log("server started");
+});
 
-  bot.start((ctx) => {
-    ctx.reply(`hello ${ctx.chat.first_name}`);
-  });
-  bot.command("test", (ctx) => {
-    ctx.reply(ctx.chat.id);
-  });
-  // register
-  bot.command("register", register);
+  // bot.launch();
 
-  bot.command("why_join", why_join);
+  // // Enable graceful stop
+  // process.once("SIGINT", () => bot.stop("SIGINT"));
+  // process.once("SIGTERM", () => bot.stop("SIGTERM"));
 
-  // check id
-  bot.command("my_id", my_info);
-
-  bot.command("when", when);
-
-  bot.command("help", help);
-
-  bot.command("present", present);
-
-  bot.command("class", class_info);
-
-  bot.command("tell_a_joke", jokeHandler);
-
-  bot.on("text", textHandler);
-
-  bot.launch();
-
-  // Enable graceful stop
-  process.once("SIGINT", () => bot.stop("SIGINT"));
-  process.once("SIGTERM", () => bot.stop("SIGTERM"));
-}
-
-main();
